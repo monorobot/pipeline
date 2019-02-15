97a22c070091acb520ad7b6dcac0b2f978ad6473
Dudás Ádám <adam.dudas@banzaicloud.com>
2019-02-15 11:47:24 +0100

    define type for distribution IDs

--- cluster/aks.go
+++ cluster/aks.go
@@ -463,7 +463,7 @@ func (c *AKSCluster) GetCloud() string {
 }
 
 // GetDistribution returns the distribution type of the cluster
-func (c *AKSCluster) GetDistribution() string {
+func (c *AKSCluster) GetDistribution() pkgCluster.DistributionID {
 	return c.modelCluster.Distribution
 }
 
